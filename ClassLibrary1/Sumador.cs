323e51342a486d4d261e14732521e13e5d1264b3
Juan <jpcuomo2000@gmail.com>
2023-12-30 01:22:44 -0300

    Agregar archivos de proyecto.

---
+++ ClassLibrary1/Sumador.cs
@@ -0,0 +1,64 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ClassLibrary1
+{
+    public class Sumador
+    {
+        private int _cantidadSumas;
+
+
+        public int CantidadSumas
+        {
+            get { return _cantidadSumas; }
+            private set { _cantidadSumas = value;}
+        }
+
+
+        public Sumador() : this(0)
+        { 
+
+        }
+
+
+        public Sumador(int cantidadSumas) 
+        {
+            _cantidadSumas = cantidadSumas;
+        }
+
+
+        public long Sumar(long a, long b)
+            {
+                CantidadSumas++;
+                return a + b;
+            }
+
+
+        public string Sumar(string a, string b)
+        {
+            CantidadSumas++;
+            return a + b;
+        }
+
+
+        public static explicit operator int(Sumador s)
+        {
+            return s.CantidadSumas;
+        }
+
+
+        public static bool operator | (Sumador s1, Sumador s2)
+        {
+            if(s1.CantidadSumas == s2.CantidadSumas)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        public static long operator + (Sumador s1, Sumador s2)
+        {
+            return s1.CantidadSumas + s2.CantidadSumas;
+        }
+    }
+}
---
+++ Ejercicio I01 - Sumador/Program.cs
@@ -0,0 +1,35 @@
+﻿using ClassLibrary1;
+
+namespace Ejercicio_I01___Sumador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador sumador1 = new Sumador();
+            Console.WriteLine($"Contador sumas: {sumador1.CantidadSumas}");
+
+            long resultado1 = sumador1.Sumar(5, 5);
+            Console.WriteLine(resultado1);
+            Console.WriteLine($"Contador sumas: {sumador1.CantidadSumas}");
+
+
+            string resultado2 = sumador1.Sumar("Hola", " que tal");
+            Console.WriteLine(resultado2);
+            Console.WriteLine($"Contador sumas: {sumador1.CantidadSumas}");
+
+            Sumador sumador2 = new Sumador();
+
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine((int)sumador1 + "*");
+
+            sumador2.Sumar("Hola", " que tal");
+            sumador2.Sumar("Hola", " como va");
+            Console.WriteLine(sumador1.CantidadSumas);
+            Console.WriteLine(sumador2.CantidadSumas);
+
+            Console.WriteLine(sumador1 + sumador2);
+            Console.WriteLine(sumador1 | sumador2);
+        }
+    }
+}
